c6f71c61ebd599f74dd584c43c27a94777554340
AnthonyRiveraI <148939728+AnthonyRiveraI@users.noreply.github.com>
2024-10-31 10:13:00 +0000

    5

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,13 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
 
+# Imprimir versión de TensorFlow para asegurar compatibilidad
 st.write(f"TensorFlow version: {tf.__version__}")
 
-# Verificar y listar archivos en la carpeta actual
-st.write("Archivos en la carpeta actual:", os.listdir("."))
-
-# Ruta directa al archivo .keras en la misma carpeta que el script
+# Definir la ruta al modelo .keras
 modelo_path = 'mobilenet_v2_model2.keras'
 
-# Verificar si el archivo del modelo existe y cargar el modelo
+# Cargar el modelo solo si el archivo existe
 if os.path.exists(modelo_path):
     try:
         model = tf.keras.models.load_model(modelo_path)
@@ -23,22 +21,21 @@ if os.path.exists(modelo_path):
 else:
     st.error("No se encontró el archivo del modelo en la carpeta actual")
 
-# Verificación de carga de archivo
-uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"], label_visibility="hidden")
+# Cargar y preprocesar la imagen para hacer una predicción
+uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None and model is not None:
     # Mostrar la imagen subida
     st.image(uploaded_file, width=300, caption="Imagen cargada")
 
-    # Preprocesamiento de la imagen para hacer la predicción
-    img = image.load_img(uploaded_file, target_size=(224, 224))  # Cambiado a (224, 224)
+    # Cargar y redimensionar la imagen
+    img = image.load_img(uploaded_file, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0
 
-    # Realizar la predicción
+    # Realizar la predicción y mostrar el resultado
     prediction = model.predict(img_array)
 
-    # Mostrar resultados
     if prediction[0][0] > 0.5:
         st.success('El modelo predice que la imagen es de un **NORMAL**.')
     else:
