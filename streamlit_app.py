550587518c95d4a5a6acbf38c054b30594c160da
AnthonyRiveraI <148939728+AnthonyRiveraI@users.noreply.github.com>
2024-10-31 09:48:33 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,14 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import wget
-import zipfile
 import os
-import contextlib
 
 st.write(f"TensorFlow version: {tf.__version__}")
 
-# Descargar y descomprimir el modelo si no existe
-def download_and_extract_model():
+# Descargar el modelo si no existe
+def download_model():
     model_url = 'https://dl.dropboxusercontent.com/s/lxb3qm5cdpa9ake5z4q9t/best_model_local.zip?rlkey=z56wewjj51qxk6djzfihi4aq1&st=04u131mj'
-    zip_path = 'best_model.keras'
+    zip_path = 'best_model.zip'
     extract_folder = 'extracted_files'
 
     # Descargar el archivo zip si no existe
@@ -24,30 +22,29 @@ def download_and_extract_model():
             st.error(f"Error al descargar el modelo: {e}")
             return None
 
-    # Descomprimir el archivo
+    # Descomprimir el archivo solo si no está ya descomprimido
     if not os.path.exists(extract_folder):
         os.makedirs(extract_folder)
-
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_folder)
+        import zipfile
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_folder)
     
-    # Devuelve la ruta a la carpeta del modelo en formato SavedModel
-    return os.path.join(extract_folder, 'best_model_local')
+    # Ruta al archivo del modelo .keras descomprimido
+    return os.path.join(extract_folder, 'best_model_local.keras')
 
 # Descargar y cargar el modelo
-modelo_path = download_and_extract_model()
+modelo_path = download_model()
 
 # Verificar si el archivo del modelo existe y cargar el modelo
-if not modelo_path or not os.path.exists(modelo_path):
-    st.error("No se encontró el archivo del modelo")
-else:
+if modelo_path and os.path.exists(modelo_path):
     try:
-        # Cargar el modelo en formato SavedModel (carpeta)
         model = tf.keras.models.load_model(modelo_path)
         st.success("Modelo cargado correctamente.")
     except Exception as e:
         st.error(f"Error al cargar el modelo: {e}")
         model = None
+else:
+    st.error("No se encontró el archivo del modelo")
 
 # Verificación de carga de archivo
 uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"], label_visibility="hidden")
@@ -62,9 +59,7 @@ if uploaded_file is not None and model is not None:
     img_array = np.expand_dims(img_array, axis=0) / 255.0
 
     # Realizar la predicción
-    with open(os.devnull, 'w') as devnull:
-        with contextlib.redirect_stdout(devnull), contextlib.redirect_stderr(devnull):
-            prediction = model.predict(img_array)
+    prediction = model.predict(img_array)
 
     # Mostrar resultados
     if prediction[0][0] > 0.5:
